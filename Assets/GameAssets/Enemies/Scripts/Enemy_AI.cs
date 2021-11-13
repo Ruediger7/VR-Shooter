2b029967e1675b5deba377b5a1f18298f4ee3b68
Nölte Bruno <bruno.noelte@stud.hs-kempten.de>
2021-11-14 00:42:55 +0100

    Migrate project to git

---
+++ Assets/GameAssets/Enemies/Scripts/Enemy_AI.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class Enemy_AI : MonoBehaviour, IEntity
+{
+    public float attackDistance = 3f;
+    public float movementSpeed = 4f;
+    public float npcHP = 100;
+    public float npcDamage = 5;
+    public float attackRate = 0.5f;
+    public Transform firePoint;
+    public GameObject npcDeadPrefab;
+    public SC_DamageReceiver player;
+
+    [HideInInspector]
+    public Transform playerTransform;
+    //[HideInInspector]
+    //public SC_EnemySpawner es;
+    NavMeshAgent agent;
+    float nextAttackTime = 0;
+    Rigidbody r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = attackDistance;
+        agent.speed = movementSpeed;
+        r = GetComponent<Rigidbody>();
+        r.useGravity = true;
+        r.isKinematic = true;
+        playerTransform = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            //if (agent.remainingDistance - attackDistance < 0.01f)
+            //{
+            //    if (Time.time > nextAttackTime)
+            //    {
+            //        nextAttackTime = Time.time + attackRate;
+
+            //        //Attack
+            //        RaycastHit hit;
+            //        if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, attackDistance))
+            //        {
+            //            if (hit.transform.CompareTag("Player"))
+            //            {
+            //                Debug.DrawLine(firePoint.position, firePoint.position + firePoint.forward * attackDistance, Color.cyan);
+
+            //                IEntity player = hit.transform.GetComponent<IEntity>();
+            //                player.ApplyDamage(npcDamage);
+            //            }
+            //        }
+            //    }
+            //}
+
+            if (Vector3.Distance(playerTransform.position, transform.position) <= 20) {
+                //Move towardst the player
+                agent.destination = playerTransform.position;
+                //Always look at player
+                transform.LookAt(new Vector3(playerTransform.transform.position.x, transform.position.y, playerTransform.position.z));
+                //Gradually reduce rigidbody velocity if the force was applied by the bullet
+                r.velocity *= 0.99f;
+            }
+    }
+
+    public void ApplyDamage(float points)
+    {
+        npcHP -= points;
+        if (npcHP <= 0)
+        {
+            //Destroy the NPC
+            GameObject npcDead = Instantiate(npcDeadPrefab, transform.position, transform.rotation);
+            //Slightly bounce the npc dead prefab up
+            npcDead.GetComponent<Rigidbody>().velocity = (-(playerTransform.position - transform.position).normalized * 8) + new Vector3(0, 5, 0);
+            Destroy(npcDead, 10);
+            //es.EnemyEliminated(this);
+            Destroy(gameObject);
+        }
+    }
+}
