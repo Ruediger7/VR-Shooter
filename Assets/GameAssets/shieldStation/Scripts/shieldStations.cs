01a55cd89f4cd1543fee2290922ee93c8dc6305c
Mayer, Richard <richard.mayer@stud.hs-kempten.de>
2021-12-08 13:37:06 +0000

    Update shieldStations.cs

--- Assets/GameAssets/shieldStation/Scripts/shieldStations.cs
+++ Assets/GameAssets/shieldStation/Scripts/shieldStations.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shieldStations : MonoBehaviour //Auf den Player ziehen
+public class shieldStations : MonoBehaviour //Auf den Player ziehen, wird zum Speichern des Arrays benötigt
 {
     public GameObject[] stations;
     // Start is called before the first frame update
     void Start()
     {
         stations = GameObject.FindGameObjectsWithTag("shieldRegenStation");
-        for (int i = 0; i < stations.Length - 1; i++)
+        for (int i = 0; i < stations.Length; i++)
         {
             stations[i].SetActive(false);
         }
